005f01df944a6c4d55048d7fb6be4e20a4b4e8f5
piyawat555 <piyawat.ju.59@ubu.ac.th>
2019-07-12 15:36:27 +0700

    Update 12/07/62

---
+++ galleryapp/index.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var win = null;
+function createWindow() {
+    win = new electron_1.BrowserWindow({ width: 800, height: 1200 });
+    win.setTitle('CSUBU Lecturers');
+    win.loadFile('index.html');
+    win.on('closed', function () { win = null; });
+}
+electron_1.app.on('ready', function () {
+    console.log('app นะ ready แล้วนะ');
+    createWindow();
+});
+// สำหรับ macOS 
+electron_1.app.on('window-all-closed', function () {
+    // ถ้าหน้าต่างทุกอันปิด
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+// ถ้ากด icon ของ app ใหม่
+electron_1.app.on('activate', function () {
+    if (win == null) {
+        createWindow();
+    }
+});
